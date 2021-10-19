8d7423f1727592e3ebb328c48c3dd331b7d815dd
dustin <dev.deokhyun@gmail.com>
2021-10-19 22:24:22 +0900

    comment pull

--- src/shared/App.js
+++ src/shared/App.js
@@ -9,14 +9,8 @@ import Footer from "../components/Footer";
 import Cart from "../pages/Cart";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-<<<<<<< HEAD
-import Comment from "../pages/CommentWrite";
-import ProductDetail from "../pages/ProductDetail";
-=======
 import ProductDetail from "../pages/ProductDetail";
 import CommentWrite from "../pages/CommentWrite";
-
->>>>>>> 0853f4d36141b0b42c32817d58fe2effb98a957f
 
 function App() {
   return (
@@ -27,15 +21,11 @@ function App() {
           <Route path="/" exact component={ProductList}></Route>
           <Route path="/signup" exact component={Signup}></Route>
           <Route path="/login" exact component={Login}></Route>
-<<<<<<< HEAD
-          <Route path="/product/:id" exact component={ProductDetail}></Route>
-=======
           <Route path="/cart" exact component={Cart}></Route>
           <Route path="/product" exact component={ProductDetail}></Route>
           <Route path="/comment" exact component={CommentWrite}></Route>
->>>>>>> 0853f4d36141b0b42c32817d58fe2effb98a957f
         </Switch>
-        <Footer/>
+        <Footer />
       </ConnectedRouter>
     </React.Fragment>
   );
