e6b3df3e4face88c87890e4321336d1711e41efc
kimbumsoo0820 <kbs2082@naver.com>
2021-10-21 15:38:59 +0900

    서치 미완

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -8,12 +8,13 @@ import { productActions } from "../redux/modules/product";
 import { Grid } from "../elements/index";
 import Banner from "../shared/img/45f975c1-e57c-403f-9f4f-1cb0c965897a.webp";
 import { apis } from "../shared/axios";
-import _ from "lodash";
+import _, { stubTrue } from "lodash";
 
 const ProductList = (props) => {
   const dispatch = useDispatch();
   const [banner, setBenner] = React.useState(null);
 
+  //배너 가져오기
   React.useEffect(() => {
     dispatch(productActions.getProductAPI());
     const fetchData = async () => {
@@ -41,7 +42,15 @@ const ProductList = (props) => {
     _handleScroll();
   };
 
-  const product_list = useSelector((state) => state.product.products.content);
+  // 그냥 productList 불러오는거랑 search 해서 불러오는거랑 구조가 다름. => 리덕스의 search flag를 이용해서 product_list
+  //에 무엇을 넣을지 삼항연산자로 넣음
+  const search = useSelector((state) => state.product.search);
+
+  const product_list =
+    search === true
+      ? useSelector((state) => state.product.products)
+      : useSelector((state) => state.product.products.content);
+
   const product_list_count = useSelector(
     (state) => state.product.products.numberOfElements
   );
--- src/redux/modules/product.js
+++ src/redux/modules/product.js
@@ -5,6 +5,7 @@ import { produce } from "immer";
 import { apis } from "../../shared/axios";
 
 const GET_PRODUCT = "GET_PRODUCT";
+const GET_SEARCH_PRODUCT = "GET_SEARCH_PRODUCT";
 const GET_BANNER = "GET_BANNER";
 const GET_MY_PRODUCT = "GET_MY_PRODUCT";
 const ADD_CART = "ADD_CART";
@@ -14,6 +15,9 @@ const addCart = createAction(ADD_CART, (data) => ({ data }));
 const getBanners = createAction(GET_BANNER, (data) => ({ data }));
 
 const getProducts = createAction(GET_PRODUCT, (data) => ({ data }));
+const getSearchProducts = createAction(GET_SEARCH_PRODUCT, (data) => ({
+  data,
+}));
 const getMyProducts = createAction(GET_MY_PRODUCT, (data) => ({ data }));
 const deleteMyProducts = createAction(DELETE_MY_PRODUCT, (id) => ({ id }));
 
@@ -22,6 +26,7 @@ const initialState = {
   myProducts: [],
   numberOfElement: "",
   is_loaded: false,
+  search: false,
 };
 //product 전체 호출
 export const getProductAPI = () => {
@@ -86,6 +91,13 @@ export default handleActions(
     [GET_PRODUCT]: (state, action) =>
       produce(state, (draft) => {
         draft.products = action.payload.data;
+        draft.search = false;
+      }),
+    [GET_SEARCH_PRODUCT]: (state, action) =>
+      produce(state, (draft) => {
+        console.log("액션빔", action);
+        draft.products = action.payload.data;
+        draft.search = true;
       }),
     [GET_MY_PRODUCT]: (state, action) =>
       produce(state, (draft) => {
@@ -107,5 +119,7 @@ const productActions = {
   getMyProductAPI,
   deleteMyProductAPI,
   addCartAPI,
+  getSearchProducts,
+  getProducts,
 };
 export { productActions };
